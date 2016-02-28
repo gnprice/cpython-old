c2363a588f32aa5a311ccd5764901b0a122af2f1
Greg Price <price@mit.edu>
2016-02-27 17:22:25 -0800

    meh

--- foo.py
+++ foo.py
@@ -1,11 +1,15 @@
 def f():
-    boom(
-        1,
-        )
+    [
+        boom()
+        for x in range(2)
+        if x
+        ]
 
 import dis
 
 def boom():
     raise RuntimeError()
 
-dis.dis(f)
+dis.dis(f.__code__.co_consts[1])
+
+f()
