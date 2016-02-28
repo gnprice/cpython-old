5544669f731c4498d3d655ae1b70639bb3ee96a2
Greg Price <price@mit.edu>
2016-02-27 21:52:27 -0800

    more demo

--- foo.py
+++ foo.py
@@ -1,18 +1,15 @@
+def f():
+    return [j
+            for i in range(3)
+            if i]
+
 def boom():
     raise RuntimeError()
-
-def f():
-    return [boom()
-            for x in range(3)
-            if x
-            for y in range(x)
-            ]
-
-f()
 
 import dis
 
 dis.dis(f)
-#print(); dis.dis(f.__code__.co_consts[1])
+print(); dis.dis(f.__code__.co_consts[1])
 
-#f()
+print()
+f()
