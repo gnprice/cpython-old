ea266908008564bb420f42a633694c603061ea5a
Greg Price <price@mit.edu>
2016-02-27 21:27:09 -0800

    more demo

--- foo.py
+++ foo.py
@@ -1,17 +1,18 @@
-def f():
-    class C():
-        def ff():
-            pass
-
-    boom(
-        1)
-
-import dis
-
 def boom():
     raise RuntimeError()
 
+def f():
+    return [boom()
+            for x in range(3)
+            if x
+            for y in range(x)
+            ]
+
+f()
+
+import dis
+
 dis.dis(f)
-print(); dis.dis(f.__code__.co_consts[1])
+#print(); dis.dis(f.__code__.co_consts[1])
 
 #f()
